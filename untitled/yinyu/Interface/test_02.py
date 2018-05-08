9452e042a2d2b689f3731f4abd086eec7191ea56
刘斌 <liubin@liuliu.io>
2018-05-08 16:15:40 +0800

    优化改动

--- untitled/yinyu/Interface/test_02.py
+++ untitled/yinyu/Interface/test_02.py
@@ -2,9 +2,7 @@
 import requests
 import unittest
 import json
-from yinyu.conf.conf import headers
-from yinyu.conf.conf import domainname
-from yinyu.conf.conf import success
+from yinyu.conf import conf
 
 class TestCase(unittest.TestCase):
     def setUp(self):
@@ -13,25 +11,25 @@ class TestCase(unittest.TestCase):
         pass
     def testcase001(self):
         # Discovery接口
-        r = requests.get(url=domainname + "/v1/discovery")
+        r = requests.get(url=conf.domainname + "/v1/discovery")
         results = json.loads(r.text)
         print(results)
-        self.assertEqual(r.status_code, success)
+        self.assertEqual(r.status_code, conf.success)
     def testcase002(self):
         # 获取大厅信息接口
-        r = requests.get(url=domainname + "/v1/lobby", headers=headers)
+        r = requests.get(url=conf.domainname + "/v1/lobby", headers=conf.headers)
         results = json.loads(r.text)
         print(results)
         self.assertEqual(results["data"]["responder_channel"]["user_count"], 100)
     def testcase003(self):
         # 获取songs列表
-        r = requests.get(url=domainname + "/v1/parttime/songs", headers=headers)
+        r = requests.get(url=conf.domainname + "/v1/parttime/songs", headers=conf.headers)
         results = json.loads(r.text)
         print(results)
-        self.assertEqual(r.status_code, success)
+        self.assertEqual(r.status_code, conf.success)
     def testcase004(self):
         #获取今日最佳歌词
-        r= requests.get(url=domainname + "/v1/hottest/lyrics",headers=headers)
+        r= requests.get(url=conf.domainname + "/v1/hottest/lyrics",headers=conf.headers)
         results = json.loads(r.text)
         print(results)
         self.assertEqual(results["data"]["sub_lyrics"],"你是我的眼，带我领略四季的变幻")
--- untitled/yinyu/Interface/tools.py
+++ untitled/yinyu/Interface/tools.py
@@ -1,9 +1,9 @@
 #coding =utf-8
 import random
 #随机生成手机号
-for _ in range(1):
-  phone=('13' +
-        str(random.randrange(1,10))+
-        ''.join( str(random.choice(range(10))) for _ in range(8) )
-         )
+class tools():
 
+    def tel(self):
+        for _ in range(1):
+           phone=('13' + str(random.randrange(1,10))+ ''.join( str(random.choice(range(10))) for _ in range(8) ))
+        return phone
